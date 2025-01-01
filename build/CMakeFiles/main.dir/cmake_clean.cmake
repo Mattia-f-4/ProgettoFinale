4f1ea7fc646233b99dabb009406b048bf7c8717b
Mattia <mattia.favretto04@gmail.com>
2025-01-01 14:09:28 +0100

    Sistemazione problematiche CmakeList e creazione main. Scrittura metodo sovraccarico. Risoluzione problemi minori con setSpegnimento. Capire come risolvere la problematica di Tempo=-1.

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/DispCicloPrefissato.cpp.obj"
+  "CMakeFiles/main.dir/src/DispCicloPrefissato.cpp.obj.d"
+  "CMakeFiles/main.dir/src/DispManuale.cpp.obj"
+  "CMakeFiles/main.dir/src/DispManuale.cpp.obj.d"
+  "CMakeFiles/main.dir/src/Dispositivo.cpp.obj"
+  "CMakeFiles/main.dir/src/Dispositivo.cpp.obj.d"
+  "CMakeFiles/main.dir/src/SistemaDomotico.cpp.obj"
+  "CMakeFiles/main.dir/src/SistemaDomotico.cpp.obj.d"
+  "CMakeFiles/main.dir/src/Tempo.cpp.obj"
+  "CMakeFiles/main.dir/src/Tempo.cpp.obj.d"
+  "CMakeFiles/main.dir/src/main.cpp.obj"
+  "CMakeFiles/main.dir/src/main.cpp.obj.d"
+  "libmain.dll.a"
+  "main.exe"
+  "main.exe.manifest"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
