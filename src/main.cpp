cf8cf4634f9c40ee8a76ef2de273a57bad969b09
Carlotto Giacomo <6190096@itisrossi.vi.it>
2025-01-06 11:43:59 +0100

    fix: gestione dispositivi con più parole nel nome

--- src/main.cpp
+++ src/main.cpp
@@ -33,30 +33,13 @@ void set_device_off(std::string device){
 }
 
 void set_device_start(std::string device, std::string start){
-    std::string delimiter = ":";
-
-    int start_h = stoi(start.substr(0, start.find(delimiter)));
-    start.erase(0, start.find(delimiter) + delimiter.length());
-    int start_m = stoi(start.substr(0, start.find(delimiter)));
-
-    Tempo t_start(start_h, start_m);
-
+    Tempo t_start(start);
     sistemaDomotico.setTimer(std::cout, device, t_start);
 }
 
 void set_device_start_stop(std::string device, std::string start, std::string stop){
-    std::string delimiter = ":";
-
-    int start_h = stoi(start.substr(0, start.find(delimiter)));
-    start.erase(0, start.find(delimiter) + delimiter.length());
-    int start_m = stoi(start.substr(0, start.find(delimiter)));
-
-    int stop_h = stoi(stop.substr(0, stop.find(delimiter)));
-    stop.erase(0, stop.find(delimiter) + delimiter.length());
-    int stop_m = stoi(stop.substr(0, stop.find(delimiter)));
-
-    Tempo t_start(start_h, start_m);
-    Tempo t_stop(stop_h, stop_m);
+    Tempo t_start(start);
+    Tempo t_stop(stop);
 
     sistemaDomotico.setTimer(std::cout, device, t_start, t_stop);
 }
@@ -74,13 +57,7 @@ void show_device(std::string device){
 }
 
 void set_time(std::string time){
-    std::string delimiter = ":";
-
-    int time_h = stoi(time.substr(0, time.find(delimiter)));
-    time.erase(0, time.find(delimiter) + delimiter.length());
-    int time_m = stoi(time.substr(0, time.find(delimiter)));
-
-    Tempo t(time_h, time_m);
+    Tempo t(time);
 
     sistemaDomotico.setTime(std::cout, t);
 }
@@ -90,11 +67,11 @@ void reset_time(){
 }
 
 void reset_timers(){
-    sistemaDomotico.resetTimers();
+    sistemaDomotico.resetTimers(std::cout);
 }
 
 void reset_all(){
-    sistemaDomotico.resetAll();
+    sistemaDomotico.resetAll(std::cout);
 }
 
 int main() {  
@@ -135,30 +112,44 @@ int main() {
         switch(mapCommands[command]){
             case Commands::set:
                 if(answer.substr(0, answer.find(delimiter)) == "time"){
-                    pattern = answer.substr(0, answer.find(delimiter));
-                    answer.erase(0, answer.find(delimiter) + delimiter.length());
-                    set_time(answer.substr(0, answer.find(delimiter)));
-                } else {
-                    device = answer.substr(0, answer.find(delimiter));
-                    answer.erase(0, answer.find(delimiter) + delimiter.length());
-                    pattern = answer.substr(0, answer.find(delimiter));
+                    std::string time = answer.substr(answer.find_last_of(delimiter) + 1);
+                    set_time(time);
 
-                    if(pattern == "on")
-                        set_device_on(device);
-                    else if ( pattern == "off")
-                        set_device_off(device);
+                } else {
+                    pattern = answer.substr(answer.find_last_of(delimiter) + 1); 
+
+                    if(pattern == "on" || pattern == "off") {
+                        answer.erase(answer.find_last_of(delimiter));
+                        device = answer;
+
+                        if(pattern == "on")
+                            set_device_on(device);
+                        else if ( pattern == "off")
+                            set_device_off(device);
+                    }
                     else {
-                        answer.erase(0, answer.find(delimiter) + delimiter.length());
-                        if(answer.substr(0, answer.find(delimiter)) == pattern)
-                            set_device_start(device, pattern);
-                        else
-                            set_device_start_stop(device, pattern, answer.substr(0, answer.find(delimiter)));
+                        answer.erase(answer.find_last_of(delimiter));
+                        if(answer.find_last_of(':') == std::string::npos){
+                            std::string start = pattern;
+                            device = answer;
+
+                            set_device_start(device, start);
+                        } else {
+                            std::string stop = pattern;
+                            std::string start = answer.substr(answer.find_last_of(delimiter) + 1);
+
+                            answer.erase(answer.find_last_of(delimiter));
+                            device = answer;
+
+                            set_device_start_stop(device, start, stop);
+                        }
                     }
                 }
                 break;
 
             case Commands::rm:
-                device = answer.substr(0, answer.find(delimiter));
+                device = answer;
+                std::cout << device << std::endl;
                 rm_device(device);
                 break;
 
@@ -166,7 +157,8 @@ int main() {
                 if(answer == "show")
                     show();
                 else{
-                    device = answer.substr(0, answer.find(delimiter));
+                    device = answer;
+                    std::cout << device << std::endl;
                     show_device(device);
                 }
                 break;
